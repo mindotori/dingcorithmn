f0ae27afd3344b45c13e65963d0ae4550573f85c
김민주 <alswn971@nate.com>
2025-04-12 00:28:33 +0900

    파이썬: 값 존재 여부

--- 1st_week/01_02_find_max_occured_alphabet.py
+++ 1st_week/01_02_find_max_occured_alphabet.py
@@ -2,19 +2,19 @@ def find_max_occurred_alphabet(string):
     # 알파벳의 빈도수를 저장한 배열을 만든다.
     # 그리고 문자열을 돌면서 해당 문자가 알파벳이라면, 알파벳을 인덱스화 시켜서 알파벳의 빈도수를 업데이트 한다.
 
-    alphabet_occurence_array = [0] * 26 # 알파벳 배열 설정
+    alphabet_occurrence_array = [0] * 26 # 알파벳 배열 설정
 
     for char in string:
         if not char.isalpha(): #알파벳인지 아닌지 검사
             continue
         arr_index = ord(char) - ord('a') # 해당 문자를 인덱스로 치환
-        alphabet_occurence_array[arr_index] += 1 # 빈도수 배열에 인덱스로 찾아가서 해당 값을 추가해준다.
+        alphabet_occurrence_array[arr_index] += 1 # 빈도수 배열에 인덱스로 찾아가서 해당 값을 추가해준다.
 
     max_occurrence = 0
     max_alphabet_index = 0
 
-    for index in range(len(alphabet_occurence_array)): #배열의 길이만큼 순회
-        alphabet_occurrence = alphabet_occurence_array[index]
+    for index in range(len(alphabet_occurrence_array)): #배열의 길이만큼 순회
+        alphabet_occurrence = alphabet_occurrence_array[index]
 
         if alphabet_occurrence > max_occurrence:
             max_occurrence = alphabet_occurrence
