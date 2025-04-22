80c0cd400cb5c5db7b64090403fd464905cb20fa
김민주 <alswn971@nate.com>
2025-04-22 17:25:03 +0900

    스택 자료구조 구현

---
+++ 3rd_week/03_06_stack.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# 스택: 한 곳에서만 자료를 넣고 뺄 수 있다
+# LIFO -> Last in first out. 가장 마지막에 넣은게 제일 빨리 나온다.
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, value):
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+
+    # pop 기능 구현 : 단순히 값을 빼는게 아니라 빼서 반환을 해줘야함.
+    def pop(self):
+        #비어 있을 경우 예외 처리
+        if self.is_empty():
+            print("stack is Empty")
+
+        delete_head = self.head # 제거할 값 반환하기 위해 저장
+        self.head = self.head.next
+        return delete_head
+
+
+    def peek(self):
+        if self.is_empty():
+            return "stack is Empty"
+
+        return self.head.data
+
+    # isEmpty 기능 구현
+    def is_empty(self):
+        return self.head is None
+
+stack = Stack()
+stack.push(4)
+print(stack.peek())
+
+stack.push(3)
+print(stack.peek())
+
+stack.push(5)
+print(stack.peek())
+
+stack.pop()
+print(stack.peek())
+
+stack.pop()
+print(stack.peek())
+
+stack.pop()
+print(stack.peek())
