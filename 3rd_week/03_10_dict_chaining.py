0ebb6e93710f2ffbb816d423846048549d10b713
김민주 <alswn971@nate.com>
2025-05-06 15:14:24 +0900

    해쉬 테이블 충돌 해결 - 체이닝 기법

--- 3rd_week/03_10_dict_chaining.py
+++ 3rd_week/03_10_dict_chaining.py
@@ -23,7 +23,7 @@ class LinkedDict:
     def __init__(self):
         self.items = []
         for i in range(8):
-            self.items.append(LinkedTuple)
+            self.items.append(LinkedTuple())
 
 
     def put(self,key, value):
@@ -36,6 +36,6 @@ class LinkedDict:
         index = hash(key) % len(self.items)
         return self.items[index].get(key) # LinkedTuple
 
-my_dict = Dict()
+my_dict = LinkedDict()
 my_dict.put("test", 3)
 print(my_dict.get("test"))
